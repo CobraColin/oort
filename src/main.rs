188567e313d4a86935adc5f08679235dc109ff0f
CobraColin <104578823+CobraColin@users.noreply.github.com>
2023-10-14 22:48:21 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 
+use std::mem::Discriminant;
+
 use oort_api::prelude::*;
 
 pub struct Ship {
@@ -29,47 +31,48 @@ impl Ship {
         predicted_position
     }
 
+    
 
-
-    fn when_will_two_objects_meet_without_acceleration(&self,position1: Vec2, velocity1: f64,position2: Vec2, velocity2: Vec2) -> f64 {
+    fn when_will_two_objects_meet_without_acceleration(&self,position1: Vec2, velocity1: Vec2) -> f64 {
         // goal find t in 
 
-        // p1 + v1*t + ½at² 
-        // =
-        // p2 + v2*t + ½at²
+        // p1 + v1*t = p2 + v2*t
 
-        // move t to the left 
-        let velocity = velocity1 - velocity2;
-        // move position aka non t to the right 
-        let position = position2 - position1;
-        // so you have v1-v2 = p2-p1
+        let dot_product_pos1_and_vel1 = position1.dot(velocity1);
 
+        let speed_squared: f64 = velocity1.length().powi(2);
 
-        // then (p2-p1)/(v1-v2) so you get T
-        let magnitude = position.length();
-        let time_to_collision = magnitude / velocity.length();
+        let discriminant = dot_product_pos1_and_vel1.powi(2) + position1.length().powi(2) * (1_000_000.0 - speed_squared);
         
-        time_to_collision
+        if discriminant > 0.0 {
+            let up_upstares = dot_product_pos1_and_vel1 + discriminant.sqrt();
+            let down_stairs = 1_000_000.0 - speed_squared;
+
+            if down_stairs != 0.0 {
+                let plus_version = up_upstares/down_stairs;
+                debug!("plus_version: {}", plus_version);
+                return plus_version
+            } 
+        }
+
+        
+
+        0.0
     }
 
 
 
-    fn predict_target(&self,target_position:Vec2,target_velocity:Vec2,acceleration:Vec2,bullet_speed:f64) -> Vec2 {
+    fn predict_target_met_gokken(&self,target_position:Vec2,target_velocity:Vec2,acceleration:Vec2,bullet_speed:f64) -> Vec2 {
         
         // get initial estimate of the time
-        let time = self.when_will_two_objects_meet_without_acceleration(
-            position(),
-            bullet_speed,
-            target_position,
-            target_velocity,
-        );
+
         
         // calculate the predicted_position with the initial estimate of the time
         let mut predicted_position = self.projectile_position(
             target_position, 
             target_velocity, 
             acceleration, 
-            time
+             self.get_distance(target())/1000.0
         );
 
         // draw line to initial estimated predicted position
@@ -89,13 +92,33 @@ impl Ship {
                 acceleration, 
                 new_time
             );
-            // draw line to the refined position
-            self.render_predicted_ship(predicted_position, (number*10) as f64);
+
+            if number == 3 {
+                debug!("gokken: {}", new_time);
+            }
+     
 
         }
         predicted_position
     }
     
+    fn predict_target_in_one_go(&self,target_position:Vec2,target_velocity:Vec2,acceleration:Vec2,bullet_speed:f64) -> Vec2 {
+        
+        // predict the time
+        let time = self.when_will_two_objects_meet_without_acceleration(target_position, target_velocity);
+
+        
+        // calculate the predicted_position with the initial estimate of the time
+        let predicted_position = self.projectile_position(
+            target_position, 
+            target_velocity, 
+            acceleration, 
+            time
+        );
+
+        predicted_position
+    }
+
     fn calculate_endpoint(
         &self, 
         predicted_position: Vec2,
@@ -119,7 +142,7 @@ impl Ship {
     fn look_at(&mut self, angle: f64) {
         // give angle difference  
 
-        let slowdown_angle = angular_velocity() * angular_velocity() / (2.0 * max_angular_acceleration());
+        let slowdown_angle = angular_velocity().powi(2) / (2.0 * max_angular_acceleration());
 
         if angle > slowdown_angle {
             torque(max_angular_acceleration());
@@ -139,15 +162,28 @@ impl Ship {
         let color = 0xFF0000; 
         let heading_color = 0x44FF00;
 
-        let bullet_speed: f64 = 1000.0/f64::sqrt(2.0);
-        let predicted_position = self.predict_target(
+        let bullet_speed: f64 = 1000.0;
+        let predicted_position: maths_rs::vec::Vec2<f64> = self.predict_target_in_one_go(
             target(),
             target_velocity(),
             acceleration_of_target,
             bullet_speed,
         );
 
-        let angle_difference = angle_diff(heading(), (predicted_position - position()).angle());
+
+        let gokken_predicted_position = self.predict_target_met_gokken(
+            target(),
+            target_velocity(),
+            acceleration_of_target,
+            bullet_speed,
+        );
+
+        let angle_difference = angle_diff(
+            heading(),
+            (
+                predicted_position - position()
+            ).angle()
+            );
         
         // calculate a vec2 that goes from our headings and has the length of our position to predicted_position
         let endpoint = self.calculate_endpoint(predicted_position);
@@ -165,13 +201,24 @@ impl Ship {
             heading_color
         );
 
+        draw_line(
+            predicted_position, 
+            target(),
+            0x800080
+        );
+        draw_line(
+            position(), 
+            predicted_position,
+            0x0000FF
+        );
+
         self.render_predicted_ship(predicted_position,10.0);
         
-        debug!("acceleration_of_target: {}", acceleration_of_target);
-        debug!("----------");
-        debug!("heading: {}", heading());
         debug!("angular_velocity: {}", angular_velocity());
-        debug!("angle_difference: {}", angle_difference);
+        debug!("----------");
+        // debug!("heading: {}", heading());
+        debug!("gokken     : {}", gokken_predicted_position);
+        debug!("niet gokken: {}", predicted_position);
         
 
         
@@ -180,6 +227,7 @@ impl Ship {
         if angle_difference > -0.005 && angle_difference < 0.005{
             fire(0);
         }
+
         self.look_at(angle_difference);
         
         
